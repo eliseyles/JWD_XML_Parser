ae6cc9587a7c77341e1ac34bd2a10e997566fafa
Alesya Khoroneko <horoneko00@gmail.com>
2020-03-23 18:07:01 +0300

    Fix pages enum

--- src/main/java/by/training/khoroneko/command/Pages.java
+++ src/main/java/by/training/khoroneko/command/Pages.java
@@ -4,8 +4,7 @@ public enum Pages {
     INDEX_JSP("page/index.jsp"),
     RESULT_JSP("page/result.jsp"),
     ERROR_PAGE_JSP("page/error.jsp"),
-    INFORMER_PAGE_JSP("page/warn.jsp")
-    ;
+    INFORMER_PAGE_JSP("page/warn.jsp");
 
     private String value;
 
